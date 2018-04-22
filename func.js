11dfb2e421d22680663fdd3e314f886222945e02
efef10 <co.efrat10@gmail.com>
2018-04-22 15:44:23 +0300

    typescript

---
+++ func.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = function (x, y) {
+    return x + y;
+};
+// export  add;
+//# sourceMappingURL=func.js.map
---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var func_1 = require("./func");
+console.log(func_1.add(5, 4));
+//# sourceMappingURL=main.js.map
